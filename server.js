098a1b0c8ae58d0e3faa19c60478631f20364ecb
Andres-Paz <marco.andres.paz@icloud.com>
2025-04-22 00:56:55 -0600

    Server actualizado

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ const express = require('express');
 const authRoutes = require('./routes/auth');
 const usuarioRoutes = require('./routes/usuarios');
 const privateRoutes = require('./routes/private');
+const salaRoutes = require('./routes/salas');
+const reservaRoutes = require('./routes/reservas');
+const funcionRoutes = require('./routes/funciones');
 const app = express();
 
 app.use(express.json());
 app.use('/auth', authRoutes);
 app.use('/usuarios', usuarioRoutes);
 app.use('/private', privateRoutes);
+app.use('/salas', salaRoutes);
+app.use('/reservas', reservaRoutes);
+app.use('/funciones', funcionRoutes);
 
 app.listen(3000, () => {
     console.log('Servidor corriendo en el puerto 3000');
